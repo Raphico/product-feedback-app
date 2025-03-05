509261352b6309c2257288ef0d16a5e5c0615d85
Raphico <raphicogit@gmail.com>
2025-03-05 17:41:47 +0100

    refactor(web): index.css -> globals.css

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./globals.css";
 import { Provider } from "react-redux";
 import store from "./lib/store.ts";
 import { RouterProvider, createRouter } from "@tanstack/react-router";

3fa1eeea5a6bc62e9ca43821d955405353e7ab20
Raphico <raphicogit@gmail.com>
2025-04-14 00:59:09 +0100

    fix: feedback page

--- packages/web/src/routes/feedback.$feedbackId.tsx
+++ packages/web/src/routes/feedback.$feedbackId.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import feedbackApi from "@/features/feedbacks/service";
+import FeedbackPage from "@/features/feedbacks/pages/feedback";
 
 export const Route = createFileRoute("/feedback/$feedbackId")({
-  component: RouteComponent,
+  loader: async ({ context: { store }, params: { feedbackId } }) => {
+    const query = store.dispatch(
+      feedbackApi.endpoints.getFeedback.initiate(feedbackId),
+    );
+    await query;
+    query.unsubscribe();
+  },
+  component: FeedbackPage,
 });
-
-function RouteComponent() {
-  return <div>Hello "/feedback/$feedbackId"!</div>;
-}

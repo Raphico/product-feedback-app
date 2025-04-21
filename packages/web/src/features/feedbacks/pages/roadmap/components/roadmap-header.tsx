1c0ebdc153248f4b03d524b59fd89f168a4a0359
Raphico <raphicogit@gmail.com>
2025-04-21 16:53:09 +0100

    refactor: roadmap go back

--- packages/web/src/features/feedbacks/pages/roadmap/components/roadmap-header.tsx
+++ packages/web/src/features/feedbacks/pages/roadmap/components/roadmap-header.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 function RoadmapHeader({ className }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <header className={cn(styles["roadmap-header"], className)}>
-      <GoBack className={styles["roadmap-header__go-back"]} />
+      <GoBack to="/" className={styles["roadmap-header__go-back"]} />
       <h1 className={cn("h1", styles["roadmap-header__title"])}>Roadmap</h1>
       <Button
         variants="primary"
